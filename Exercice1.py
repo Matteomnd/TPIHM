a4ed6f945d6b59c1dfa9170d6dce267daf725ee1
PC Mattéo <matteo.menard@isen-ouest.yncrea.fr>
2020-05-27 14:02:35 +0200

    aligner

--- Exercice1.py
+++ Exercice1.py
@@ -1,15 +1,15 @@
-from PySide2.QtWidgets import QWidget,QVBoxLayout, QPushButton, QCheckBox, QApplication, QLineEdit
+from PySide2.QtWidgets import *
 class Window(QWidget):
     def __init__(self):
         QWidget.__init__(self)
-        self.layout = QVBoxLayout()
+        self.layout = QGridLayout()
         self.button1= QPushButton("valider")
         self.line = QLineEdit()
         self.button2 = QPushButton("annuler")
 
-        self.layout.addWidget(self.button1)
-        self.layout.addWidget(self.line)
-        self.layout.addWidget(self.button2)
+        self.layout.addWidget(self.button1,1,0)
+        self.layout.addWidget(self.line,0,0,1,2)
+        self.layout.addWidget(self.button2,1,1)
 
         self.setLayout(self.layout)
 
